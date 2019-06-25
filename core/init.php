65b2ee374415947a12fada45832baa2b84e5a5e6
Mark T <ratrateroo@gmail.com>
2019-06-26 02:23:58 +0800

    added some database features

--- core/init.php
+++ core/init.php
@@ -10,16 +10,18 @@ $GLOBALS['config'] = array(
     ),
     'remember' => array(
         'cookie_name' => 'hash',
-        'cookie_expiry' => 604800
+        'cookie_expiry' => '604800'
     ),
     'session' => array(
         'session_name' => 'user'
     )
     );
 
-spl_autoload_register(function($class) {
+spl_autoload_register(
+    function($class) {
     require_once 'classes/'. $class . '.php';
-});
+}
+);
 
 require_once 'functions/sanitize.php';
 
--- index.php
+++ index.php
@@ -1,3 +1,23 @@
 <?php 
     require_once 'core/init.php';
+   
+    // $user = DB::getInstance()->query("SELECT username FROM users WHERE username = ?", array('alex'));
+    // $user = DB::getInstance()->get('users', array('username', '=', 'alex'));
+    $user = DB::getInstance()->query("SELECT * FROM users");
+
+    if($user->error()) {
+        echo 'No user';
+    } else {
+        echo 'OK!';
+    }
+
+    if(!$user->count()) {
+        echo 'No user';
+    } else {
+        // foreach($user->results() as $user) {
+        //     echo $user->username, '<br>';
+        // }
+
+        echo $user->first()->username;
+    }
 ?>
